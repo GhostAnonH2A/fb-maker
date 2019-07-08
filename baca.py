e2c0e6f031a6f44996e111d85f72443965dad37c
GhostAnonH2A <49526865+GhostAnonH2A@users.noreply.github.com>
2019-07-07 22:06:32 -0500

    Add files via upload

---
+++ baca.py
@@ -0,0 +1,13 @@
+# Bawa enjoy aja :)
+print('=================================')
+print('+ Coded : GhostAnonH2A          +')
+print('+ Thank : To All :)             +')
+print('=================================')
+#
+web= 'https://github.com/GhostAnonH2A/fb-maker.git' ;
+print('---------------------------------')
+print('| Nih Klo ada yang error :V     |')
+print('| Support Termoss Or Linuxer    |')
+print('---------------------------------')
+#
+print('apt install python') ; print('apt install python2') ; print('pip install --upgrade pip') ; print('pip2 install --upgrade pip') ; print('pip install BeatifulSoup4') ; print('pip install requests') ; print('pip2 install requests') ; print('git pull') ; print('cd fb-maker') ; print('Termux --> python2 fb-maker.py') ; print('Linux --> python fb-maker.py') ; print('DONE !!! ENJOY ')
